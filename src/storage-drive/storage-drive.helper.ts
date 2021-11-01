b44b25a0de9f7ffeb43462a471bb0f162cacd12f
Daniel Coelho <daniel.coelho@turbinekreuzberg.com>
2021-11-01 16:20:42 +0000

    Add debounce on refreshing data

--- src/storage-drive/storage-drive.helper.ts
+++ src/storage-drive/storage-drive.helper.ts
@@ -1,3 +1,14 @@
 export function getStorageItemSize(adapter: Storage, key: string): number {
   return (key.length + (adapter?.[key] || []).length) * 2; // "* 2" because char in js is stored as UTF-16 (takes 2 bytes)
 }
+
+export const debounce = (callback: Function, context: unknown, timeout: number): CallableFunction => {
+  let timeoutId: NodeJS.Timeout;
+
+  return (...args: unknown[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback.apply(context, args);
+    }, timeout);
+  };
+}
